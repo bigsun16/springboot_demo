98fdd03ee53089be8d155c7b42114167ec204e4b
bigsun16 <bigsun16@126.com>
2021-01-27 13:08:13 +0800

    review code

--- springboot_webclient/src/main/java/com/qihui/sun/springboot_webclient/controller/HelloWorldController.java
+++ springboot_webclient/src/main/java/com/qihui/sun/springboot_webclient/controller/HelloWorldController.java
@@ -1,13 +1,10 @@
 package com.qihui.sun.springboot_webclient.controller;
 
-import com.qihui.sun.springboot_webclient.utils.WebClientRequestUtil;
 import com.qihui.sun.springboot_webclient.vo.StudentProperties;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 public class HelloWorldController {
@@ -28,21 +25,6 @@ public class HelloWorldController {
     @ApiOperation("test springboot hello world")
     public String helloWorld() {
         return studentProperties.getName() + "::" + studentProperties.getAge();
-    }
-
-    @GetMapping("/login")
-    @ApiOperation("login")
-    public Map testWebClient() {
-        WebClientRequestUtil.login();
-        return null;
-    }
-
-
-    @GetMapping("/logout")
-    @ApiOperation("logout")
-    public Map testWebClient2() {
-        WebClientRequestUtil.logout("");
-        return null;
     }
 
 }
--- springboot_webclient/src/test/java/com/qihui/sun/springboot_webclient/SpringbootWebclientApplicationTests.java
+++ springboot_webclient/src/test/java/com/qihui/sun/springboot_webclient/SpringbootWebclientApplicationTests.java
@@ -1,8 +1,12 @@
 package com.qihui.sun.springboot_webclient;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootWebclientApplicationTests {
 
+    @Test
+    public void test(){
+    }
 }
