5af31c1b7b106c9e4a5b06501917af8d0e8c1bd4
bigsun16 <bigsun16@126.com>
2021-02-24 14:16:10 +0800

    include a new yml

--- springboot_webclient/src/test/java/com/qihui/sun/springboot_webclient/SpringbootWebclientApplicationTests.java
+++ springboot_webclient/src/test/java/com/qihui/sun/springboot_webclient/SpringbootWebclientApplicationTests.java
@@ -1,12 +1,19 @@
 package com.qihui.sun.springboot_webclient;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
+import org.springframework.test.annotation.Commit;
 
 @SpringBootTest
-class SpringbootWebclientApplicationTests {
+class SpringBootWebclientApplicationTests {
+
+    @Value("${aa.bb}")
+    private String text;
 
     @Test
     public void test(){
+        System.out.println(text);
     }
 }
